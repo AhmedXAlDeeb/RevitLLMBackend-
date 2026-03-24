79e2946cd51da1f896444862fa98cfbf8e7005f5
ahmedxaldeeb <ahmedaldiep@gmail.com>
2026-03-25 00:05:30 +0200

    ph 1,2&3

---
+++ RevitLLMAddin/RevitLLMAddin/Models/ComplianceResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitLLMAddin.Models
+{
+    public class ComplianceResult
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ RevitLLMAddin/RevitLLMAddin/Services/RoomService.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.Revit.DB;
+using RevitLLMAddin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitLLMAddin.Services
+{
+    public class RoomService
+    {
+        public List<RoomModel> GetRooms(Document doc)
+        {
+            var rooms = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Rooms)
+                .WhereElementIsNotElementType();
+
+            var result = new List<RoomModel>();
+
+            foreach (var room in rooms)
+            {
+                double area = UnitUtils.ConvertFromInternalUnits(
+                    room.get_Parameter(BuiltInParameter.ROOM_AREA).AsDouble(),
+                    UnitTypeId.SquareMeters);
+
+                if (area <= 0) continue;
+
+                string name = room.Name ?? "Unknown";
+
+                result.Add(new RoomModel
+                {
+                    Id = (int)room.Id.Value,
+                    Name = room.Name,
+                    Area = area
+                });
+            }
+
+            return result;
+        }
+    }
+}
