fece929765ef199c2a79d459e028db64e259f061
ahmedxaldeeb <ahmedaldiep@gmail.com>
2026-03-25 23:25:03 +0200

    add requirements

--- vectorstore/faiss_store.py
+++ vectorstore/faiss_store.py
@@ -1,9 +1,9 @@
 from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings
+from langchain_ollama import OllamaEmbeddings
 
 class FAISSStore:
-    def __init__(self):
-        self.embeddings = OpenAIEmbeddings()
+    def __init__(self, model: str = "nomic-embed-text", base_url: str = "http://localhost:11434"):
+        self.embeddings = OllamaEmbeddings(model=model, base_url=base_url)
 
     def create_store(self, documents):
         vectorstore = FAISS.from_documents(documents, self.embeddings)
